6b0091525d4a19d0cc54a91a1715159775681236
Dhanya <dhanyasri0510@gmail.com>
2025-07-28 17:00:11 +0530

    fix: - removed unwanted files

--- src/api/parser.js
+++ src/api/parser.js
@@ -1,14 +1,4 @@
-export function parseProducts(product) {
-  return {
-    id: product.id,
-    title: product.title,
-    price: product.price,
-    images: product.images,
-    description: product.description,
-    category: product.category,
-    brand: product.brand,
-  };
-}
+
 export function parseUser(user) {
   return {
     username: user.username,
