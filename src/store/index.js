4c5e7ce784df74ba3ca7c31d5aeae8d70d4b82bd
Dhanya <dhanyasri0510@gmail.com>
2025-07-28 16:02:18 +0530

    fix: - removed  imported product module

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { users } from './modules/user';
-import { products } from './modules/product';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     users,
-    products
   },
 });
