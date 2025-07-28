3424a020a9b19e16692e23986622b4437979e527
Dhanya <dhanyasri0510@gmail.com>
2025-07-28 07:52:55 +0530

    feat: Implement product data fetching

---
+++ src/api/products.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const products = {
+  /**
+   * retrieves product data 
+   * @returns returns product data 
+   */
+
+  async fetchAllProducts() {
+    let BASE_URL = 'https://dummyjson.com/products';
+
+    try {
+      const { data } = await axios.get(BASE_URL);
+      return {
+        data: data.products,
+        total: data.total,
+      };
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  },
+};
