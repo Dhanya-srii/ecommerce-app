dc790378cb94e58ceb4d3bd1b399653968572e08
Dhanya <dhanyasri0510@gmail.com>
2025-07-29 16:07:57 +0530

    feat: - imported lazy file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './route';
 import store from './store';
+import { lazy } from './directives/lazy';
+
 Vue.config.productionTip = false;
+Vue.directive('lazy', lazy);
 
 new Vue({
   render: (h) => h(App),
