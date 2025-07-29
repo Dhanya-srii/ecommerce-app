8db32770ace9728575cf454ca6ba1d090eddbabd
Dhanya <dhanyasri0510@gmail.com>
2025-07-29 16:45:03 +0530

    feat: - added  API to fetch products by price

--- src/api/products.js
+++ src/api/products.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { parseProduct } from './parser';
 
 export const products = {
-/**
- * retrieves a list of all products.
- * @returns A list containing data of all available products.
- */
+  /**
+   * retrieves a list of all products.
+   * @returns A list containing data of all available products.
+   */
 
   async fetchAllProducts() {
     let BASE_URL = 'https://dummyjson.com/products';
@@ -16,6 +16,22 @@ export const products = {
         data: data.products.map(parseProduct),
         total: data.total,
       };
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  },
+  /**
+   * retrieves the product data by price
+   * @param {string} sort
+   * @returns returns the product data by price .
+   */
+
+  async fetchProductsByPrice(sort) {
+    try {
+      const { data } = await axios.get(
+        `https://dummyjson.com/products?sortBy=title&order=${sort}`
+      );
+      return { data: data.products.map(parseProduct) };
     } catch (err) {
       throw new Error(err.message);
     }
