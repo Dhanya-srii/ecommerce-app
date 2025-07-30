dc0de21d26a408e80508ede881c42949dff8cf2f
Dhanya <dhanyasri0510@gmail.com>
2025-07-30 16:52:06 +0530

    fix: - imported 'checkbox-group' from element-ui library

--- src/main.js
+++ src/main.js
@@ -10,10 +10,11 @@ Vue.directive('lazy', lazy);
 import { Rate } from 'element-ui';
 import { Button } from 'element-ui';
 import { Checkbox } from 'element-ui';
+import { CheckboxGroup } from 'element-ui';
 Vue.use(Rate);
 Vue.use(Button);
 Vue.use(Checkbox);
-
+Vue.use(CheckboxGroup);
 new Vue({
   render: (h) => h(App),
   router,
