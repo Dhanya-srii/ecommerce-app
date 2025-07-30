c134446e3169eb51b410c57dc6e67c3992099f6a
Dhanya <dhanyasri0510@gmail.com>
2025-07-30 14:51:40 +0530

    feat: - added category filter functionality

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -9,7 +9,10 @@
       <div
         class="sub-filter-sort-panel display-flex align-items-center justify-content-space-between"
       >
-        <button class="filter-sort-panel-button">
+        <button
+          @click="toggleFilter()"
+          class="filter-sort-panel-button"
+        >
           <span>FILTERS</span><i class="ri-equalizer-3-line"></i></button
         ><select class="select-product">
           <option
@@ -29,16 +32,43 @@
       :totalProducts="listProducts.length"
     />
     <div
+      v-if="selectedCategories.length"
+      class="filters display-flex justify-content-center"
+    >
+      <div class="filter-container display-flex">
+        <button
+          v-for="(category, index) in selectedCategories"
+          :key="'Cat-' + index"
+          class="filter-pill display-flex align-items-center justify-content-space-between"
+        >
+          <span>{{ category }}</span>
+          <button
+            class="remove-filter"
+            @click="removeCategory(category)"
+          >
+            <i class="ri-close-line"></i>
+          </button>
+        </button>
+      </div>
+
+      <button
+        class="clear-filters display-flex align-items-center justify-content-space-between"
+        v-if="selectedCategories.length > 0"
+        @click="clearAllFilters()"
+      >
+        <span>Clear All</span> <i class="ri-close-circle-line"></i>
+      </button>
+    </div>
+    <div
       v-if="listProducts.length > 0"
       class="products display-flex justify-content-start"
     >
       <product-card
         v-for="(product, index) in listProducts"
         :key="index"
-        :productData="product"
+        :productList="product"
       />
     </div>
-
     <div v-else>
       <p
         class="product-status-message display-flex justify-content-center align-items-center"
@@ -49,7 +79,7 @@
   </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions, mapMutations } from 'vuex';
 import ProductCard from './ProductCard.vue';
 import ProductSpecifications from './ProductSpecifications.vue';
 
@@ -62,10 +92,12 @@ export default {
 
   computed: {
     ...mapState({
-      productData: (state) => state.product.productData,
+      productList: (state) => state.product.productList,
+      selectedCategories: (state) => state.product.selectedCategories,
+      showFilter: (state) => state.product.showFilter,
     }),
     listProducts() {
-      return this.productData;
+      return this.productList;
     },
   },
 
@@ -77,11 +109,29 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['getAllProducts']),
+    ...mapActions(['getAllProducts', 'getAllProductsByCategories']),
+    ...mapMutations([
+      'setProductList',
+      'clearSelectedCategories',
+      'removeOneSelectedCategory',
+      'toggleFilter',
+      'setTotalProducts',
+    ]),
+    clearAllFilters() {
+      this.clearSelectedCategories();
+      this.getAllProductsByCategories();
+    },
+
+    removeCategory(category) {
+      this.removeOneSelectedCategory(category);
+      this.getAllProductsByCategories();
+    },
   },
 };
 </script>
 <style scoped lang="scss">
+@use '/src/styles/abstracts/_variables.scss' as *;
+
 .filter-sort-panel {
   padding: 1rem 2rem;
   width: 100vw;
@@ -91,8 +141,49 @@ export default {
     font-size: 14px;
   }
 }
+.filters {
+  background-color: $secondary-color;
+  padding: 16px 0px;
+  width: 1600px;
+}
 
+.filter-container {
+  overflow-x: auto;
+  flex-wrap: nowrap;
+  flex: 1;
+  gap: 8px;
+  padding-bottom: 4px;
+}
+.filter-pill,
+.clear-filters {
+  background-color: rgb(238, 238, 238);
+  padding: 0 18px;
+  white-space: nowrap;
+  text-transform: capitalize;
+  font-size: 14px;
+}
 .sub-filter-sort-panel {
   min-width: 1600px;
+}
+
+.remove-filter,
+.ri-close-circle-line {
+  background: none;
+  border: none;
+  font-weight: bold;
+  font-size: 1.2rem;
+  cursor: pointer;
+  padding: 4px;
+  font-weight: 300;
+  text-align: left;
+}
+
+.clear-filters {
+  flex-shrink: 0;
+  font-weight: 800;
+  background-color: $secondary-color;
+  cursor: pointer;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
