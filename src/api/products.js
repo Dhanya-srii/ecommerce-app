a6ec305c90dc0d92ce9396644d1ec3a38cb9334b
Dhanya <dhanyasri0510@gmail.com>
2025-07-30 12:58:45 +0530

    feat: - added categoryNames and productsCategories

--- src/api/products.js
+++ src/api/products.js
@@ -38,4 +38,48 @@ export const products = {
       throw new Error(err.message);
     }
   },
+
+  /**
+   *  retrieves the names of all available product categories.
+   * @returns {Promise<String[]>} A promise that resolves to an array of category names (strings).
+   */
+
+  async fetchCategoryNames() {
+    try {
+      const response = await axios.get(
+        'https://dummyjson.com/products/categories'
+      );
+
+      return response.data;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  },
+
+  /**
+   *  retrieves product data for the specified categories.
+   * This method fetches products that belong to one or more of the categories provided in the `categoryList`.
+   * @param {string[]} categoryList An array of strings, where each string is the name of a category to filter products by.
+   * @returns {Promise<{ products: Array<Object>, totalProducts: number }>} A promise that resolves to an object
+   * containing an array of product objects (`products`) and the total number of products found (`totalProducts`)
+   * for the given categories.
+   */
+  async fetchProductsCategories(categoryList) {
+    const allProducts = [];
+    let totalProducts = 0;
+
+    try {
+      for (const category of categoryList) {
+        const response = await axios.get(
+          `https://dummyjson.com/products/category/${category}`
+        );
+        const products = response.data.products;
+        totalProducts += products.length;
+        allProducts.push(...products.map(parseProducts));
+      }
+      return { products: allProducts, totalProducts };
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  },
 };
