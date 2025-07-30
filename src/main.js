1a60903fd2f80caaf23c7760553ac0222e14e000
Dhanya <dhanyasri0510@gmail.com>
2025-07-30 14:54:12 +0530

    update: - imported element-ui library

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import { lazy } from './directives/lazy';
 Vue.config.productionTip = false;
 Vue.directive('lazy', lazy);
 
+import { Rate } from 'element-ui';
+import { Button } from 'element-ui';
+import { Checkbox } from 'element-ui';
+Vue.use(Rate);
+Vue.use(Button);
+Vue.use(Checkbox);
+
 new Vue({
   render: (h) => h(App),
   router,
