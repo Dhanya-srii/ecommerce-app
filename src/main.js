a0796b08a8297415ab7fb0f9a13841ce895c44da
Dhanya <dhanyasri0510@gmail.com>
2025-08-01 12:20:07 +0530

    feat: - added Element UI Rate component

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import App from './App.vue';
 import router from './route';
 import store from './store';
 import { lazy } from './directives/lazy';
-import { Option, Select } from 'element-ui';
+import { Option, Select, Rate } from 'element-ui';
 
 Vue.config.productionTip = false;
 Vue.directive('lazy', lazy);
 Vue.use(Select);
 Vue.use(Option);
+Vue.use(Rate);
 new Vue({
   render: (h) => h(App),
   router,
