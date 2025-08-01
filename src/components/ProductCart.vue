493d833bc4f1a9a2d1ddc659d15c34cf697448a4
Dhanya <dhanyasri0510@gmail.com>
2025-08-01 15:26:39 +0530

    feat: - created ProductCart and ProductCartList components

---
+++ src/components/ProductCart.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="cart-item display-flex justify-content-start">
+    <div
+      class="cart-item-image display-flex align-items-center flex-direction-column"
+    >
+      <img
+        class="cart-image"
+        :src="product.images[0]"
+        alt="Product image"
+      />
+      <span class="cart-item-stock"><i class="ri-check-line"></i>IN STOCK</span>
+    </div>
+    <div class="cart-item-content display-flex justify-content-space-between">
+      <div class="cart-item-info">
+        <p class="cart-item-title">{{ product.title }}</p>
+        <div class="cart-item-details">
+          <p class="about-product">
+            <span class="about-product-title">Color:</span> High Risk Red-Puma
+            Black
+          </p>
+          <p class="about-product">
+            <span class="about-product-title">Size:</span> UK 8
+          </p>
+          <p class="about-product">
+            <span class="about-product-title">Style number:</span> 311887_01
+          </p>
+        </div>
+
+        <div
+          class="addCart-details display-flex align-items-center justify-content-space-between"
+        >
+          <button
+            class="counter-button"
+            @click="updateCart({ id: product.id, quantityChange: -1 })"
+          >
+            -
+          </button>
+          <span>{{ product.quantity }}</span>
+          <button
+            class="counter-button"
+            @click="updateCart({ id: product.id, quantityChange: 1 })"
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div
+        class="cart-item-side display-flex align-items-center flex-direction-column"
+      >
+        <p class="cart-item-price">
+          ${{ (product.price * product.quantity).toFixed(2) }}
+        </p>
+        <div class="cart-item-actions">
+          <button class="user-btn"><i class="ri-edit-line"></i></button>
+          <button
+            class="user-btn"
+            @click="updateCart({ id: product.id, remove: true })"
+          >
+            <i class="ri-delete-bin-line"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    ...mapActions(['updateCart']),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use '/src/styles/abstracts/_variables.scss' as *;
+
+.cart-item {
+  gap: 16px;
+  padding: 16px;
+  border: 1px solid #e2dddd;
+  height: 230px;
+}
+
+.cart-item-image {
+  width: 20%;
+  background-color: #eeee;
+
+  .cart-image {
+    height: 150px;
+    width: 180px;
+  }
+}
+
+.cart-item-stock {
+  margin-top: 8px;
+  font-size: 10px;
+  text-align: center;
+  color: #4d7d04;
+  border: 1px solid #4d7d04;
+  padding: 6px;
+  background-color: #ffff;
+  border-radius: 12px;
+
+  .ri-check-line {
+    background-color: #4d7d04;
+    color: $secondary-color;
+    border-radius: 50%;
+    margin: 4px;
+  }
+}
+
+.cart-item-content {
+  flex: 1;
+}
+
+.cart-item-info {
+  flex: 1;
+}
+
+.cart-item-title {
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-bottom: 16px;
+}
+
+.about-product {
+  font-weight: 300;
+  padding: 4px 0;
+
+  .about-product-title {
+    color: gray;
+    font-weight: 200;
+  }
+}
+
+.cart-item-quantity {
+  margin-top: 16px;
+  padding: 16px 32px;
+  font-size: 16px;
+}
+
+.cart-item-side {
+  gap: 32px;
+}
+
+.cart-item-price {
+  font-size: 18px;
+}
+
+.user-btn {
+  border: none;
+  background: none;
+  font-size: 24px;
+  margin: 4px 8px;
+
+  .ri-edit-line,
+  .ri-delete-bin-line {
+    font-weight: 300;
+  }
+}
+</style>
---
+++ src/components/ProductCartList.vue
@@ -0,0 +1,255 @@
+<template>
+  <div class="cart display-flex align-items-center justify-content-center">
+    <p
+      class="product-status-message display-flex justify-content-center align-items-center"
+      v-if="getAddedCartProducts.products.length === 0"
+    >
+      No items in cart.
+    </p>
+    <div
+      v-else
+      class="cart-wrapper display-flex flex-direction-column"
+    >
+      <p class="cart-title">
+        My Shopping Cart
+        <span class="item-count"
+          >({{ getAddedCartProducts.products.length || 0 }})</span
+        >
+      </p>
+
+      <div class="cart-content display-flex">
+        <div class="cart-items display-flex flex-direction-column">
+          <product-cart
+            v-for="product in getAddedCartProducts.products"
+            :key="product.id"
+            :product="product"
+          />
+        </div>
+
+        <div class="cart-summary">
+          <p class="free-shipping">
+            <i class="ri-truck-line"></i>
+            {{ "You've earned free shipping" }}
+          </p>
+          <p class="free-returns">
+            <i class="ri-clockwise-line"></i>
+            Free returns on all qualifying orders
+          </p>
+          <select class="promo-select">
+            <option value="">Apply a promo card</option>
+          </select>
+
+          <div class="order-breakdown">
+            <p class="discount-info display-flex justify-content-space-between">
+              <span class="discount-title">Extra 15% off applied</span>
+              <span class="discount-amount">{{
+                (
+                  getAddedCartProducts.total -
+                  getAddedCartProducts.discountedTotal
+                ).toFixed(2)
+              }}</span>
+            </p>
+            <div class="price-details">
+              <p
+                class="sub-price-details display-flex justify-content-space-between"
+              >
+                <span>Subtotal</span>
+                <span>${{ getAddedCartProducts.total.toFixed(2) }}</span>
+              </p>
+              <p
+                class="sub-price-details display-flex justify-content-space-between"
+              >
+                <span>shipping costs</span>
+                <span>Free</span>
+              </p>
+              <p
+                class="sub-price-details display-flex justify-content-space-between"
+              >
+                <span>order discount</span>
+                <span
+                  >${{
+                    (
+                      getAddedCartProducts.total -
+                      getAddedCartProducts.discountedTotal
+                    ).toFixed(2)
+                  }}</span
+                >
+              </p>
+            </div>
+          </div>
+
+          <div class="total display-flex justify-content-space-between">
+            <p>
+              Grand Total
+              <span class="gst-note">Prices include GST</span>
+            </p>
+            <p>${{ getAddedCartProducts.discountedTotal }}</p>
+          </div>
+
+          <button class="checkout-button">Checkout</button>
+          <p class="terms-text">
+            By continuing, I confirm that I have read and accept the
+            <a
+              class="condition"
+              href="#"
+              >Terms and Conditions</a
+            >
+            and the
+            <a
+              class="condition"
+              href="#"
+              >Privacy Policy</a
+            >.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductCart from './ProductCart.vue';
+import { mapState } from 'vuex';
+export default {
+  name: 'ProductCartList',
+  components: { ProductCart },
+  computed: {
+    ...mapState({
+      getAddedCartProducts: (state) => state.product.cartData,
+    }),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use '/src/styles/abstracts/_variables.scss' as *;
+
+.cart {
+  width: 100vw;
+}
+.cart-wrapper {
+  min-width: 1600px;
+  min-height: 700px;
+  gap: 32px;
+  margin: 24px;
+}
+
+.cart-title {
+  font-size: 32px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+.cart-content {
+  gap: 24px;
+}
+
+.cart-items {
+  width: 60%;
+  gap: 16px;
+}
+
+.item-count {
+  color: gray;
+  font-weight: 300;
+}
+
+.cart-summary {
+  width: 40%;
+}
+
+.free-shipping,
+.free-returns {
+  border: 1px solid #d0caca;
+  text-align: center;
+  padding: 8px;
+  font-size: 14px;
+  text-transform: uppercase;
+  margin-bottom: 8px;
+
+  .ri-clockwise-line {
+    padding: 16px;
+    font-size: 18px;
+  }
+}
+
+.free-shipping {
+  color: green;
+}
+
+.free-returns {
+  color: gray;
+}
+
+.promo-select {
+  width: 100%;
+  margin: 16px 0;
+  padding: 16px;
+  border: none;
+  font-size: 18px;
+  background-color: #eeeeee;
+  font-weight: 400;
+  text-transform: uppercase;
+}
+
+.order-breakdown {
+  border-top: 1px solid #cac8c8;
+  border-bottom: 1px solid #cac8c8;
+  padding: 16px 0;
+}
+
+.discount-info {
+  text-transform: uppercase;
+
+  .discount-amount,
+  .discount-title {
+    font-weight: 300;
+    font-size: 14px;
+  }
+}
+
+.discount-amount {
+  color: green;
+}
+
+.price-details {
+  margin-top: 24px;
+  color: #5e5d5d;
+  font-size: 14px;
+}
+
+.sub-price-details,
+.total {
+  padding: 4px 0;
+  text-transform: uppercase;
+}
+
+.total {
+  margin: 38px 0;
+
+  .gst-note {
+    font-size: 15px;
+    color: gray;
+  }
+}
+
+.checkout-button {
+  width: 100%;
+  background-color: $primary-color;
+  color: $secondary-color;
+  padding: 16px;
+  font-size: 18px;
+  text-transform: uppercase;
+}
+
+.terms-text {
+  font-size: 12px;
+  margin: 16px 0;
+  font-weight: 300;
+  color: $primary-color;
+
+  .condition {
+    color: $primary-color;
+  }
+}
+</style>
