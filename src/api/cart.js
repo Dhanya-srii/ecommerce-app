3d9b652f959f768e3464e2017eb654600ac9dc3e
Dhanya <dhanyasri0510@gmail.com>
2025-08-06 12:49:44 +0530

    fix: - renamed "productData" to "cartData"

--- src/api/cart.js
+++ src/api/cart.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 export const cart = {
   /**
    *  Adds a product to the cart.
-   * @param {object} productData The data for the product to be added.
+   * @param {object} cartData The data for the product to be added.
    * @returns {Promise<object>} A promise that resolves with the added cart product data.
    */
 
-  async addCart(productData) {
+  async addCart(cartData) {
     try {
       const { data } = await axios.post(
         'https://dummyjson.com/carts/add',
-        productData,
+        cartData,
         {
           headers: { 'Content-Type': 'application/json' },
         }
